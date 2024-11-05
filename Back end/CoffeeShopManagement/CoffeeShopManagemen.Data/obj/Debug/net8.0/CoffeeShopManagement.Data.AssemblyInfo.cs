097b981bf454a5e554d92051c1accbadd5d84951
Dang Quoc Vuong <vuongdqhe170389@fpt.edu.vn>
2024-11-05 10:15:29 +0700

    Create GenericRepository with basic operation and UnitOfWork

--- Back end/CoffeeShopManagement/CoffeeShopManagemen.Data/obj/Debug/net8.0/CoffeeShopManagement.Data.AssemblyInfo.cs
+++ Back end/CoffeeShopManagement/CoffeeShopManagemen.Data/obj/Debug/net8.0/CoffeeShopManagement.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShopManagement.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed640d4174ccef2ee5243c37f6137d23fd64bb6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53da27f6da7455c85e9fd82549bc124eab1c0aa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShopManagement.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShopManagement.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
