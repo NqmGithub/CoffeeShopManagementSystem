eadf2ed380acd99907e9b64b6a783fa6002bff43
Nam <nguyennqm2406@gmail.com>
2024-11-05 22:10:47 +0700

    Added unauthorized component and route guard

--- Front end/CoffeeShop/src/routers/app.routes.ts
+++ Front end/CoffeeShop/src/routers/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../views/login/login.component';
 import { HomeComponent } from '../views/home/home.component';
+import { UnauthorizedComponent } from '../views/unauthorized/unauthorized.component';
 
 export const routes: Routes = [
     {path:'home', component:HomeComponent},
-    {path:'login', component:LoginComponent}
+    {path:'login', component:LoginComponent},
+    {path:'unauthorized', component:UnauthorizedComponent}
 ];
