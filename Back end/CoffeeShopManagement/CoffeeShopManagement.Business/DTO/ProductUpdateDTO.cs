07e1146cdecabc3ba44ff1c312b5e51e79163f52
Dang Quoc Vuong <vuongdqhe170389@fpt.edu.vn>
2024-11-06 14:21:10 +0700

    create API product

---
+++ Back end/CoffeeShopManagement/CoffeeShopManagement.Business/DTO/ProductUpdateDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopManagement.Business.DTO
+{
+    public class ProductUpdateDTO
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "ProductName is required")]
+        public string ProductName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Category is required")]
+        public string CategoryName { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.1, int.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0.")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Thumbnail is required")]
+        public string? Thumbnail { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public string Status { get; set; }
+    }
+}
---
+++ Back end/CoffeeShopManagement/CoffeeShopManagement/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using CoffeeShopManagement.Business.DTO;
+using CoffeeShopManagement.Business.ServiceContracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopManagement.WebAPI.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+      
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(Guid id)
+        {
+            var product = await _productService.GetProductById(id);
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAlllProducts()
+        {
+            var products =  _productService.GetListProduct();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(ProductCreateDTO productCreateDTO)
+        {
+            var result = await _productService.CreateProduct(productCreateDTO);
+            return Ok(result);
+        }
+
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> UpdateUser(Guid id, ProductUpdateDTO productUpdateDTO)
+        {
+            var result = await _productService.UpdateProduct(productUpdateDTO);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> DeactiveUser(Guid id, [FromQuery] string status)
+        {
+            var result = await _productService.ChangeStatusProductById(id,status);
+            return Ok(result);
+        }
+    }
+}
+
