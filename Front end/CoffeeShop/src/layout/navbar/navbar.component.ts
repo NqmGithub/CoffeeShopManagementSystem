3e78a8c58c36bb3ae63df7b5c457902cdcf4c8af
Nam <nguyennqm2406@gmail.com>
2024-11-18 20:51:34 +0700

    fix navbar

--- Front end/CoffeeShop/src/layout/navbar/navbar.component.ts
+++ Front end/CoffeeShop/src/layout/navbar/navbar.component.ts
@@ -6,6 +6,7 @@ import { Router,RouterModule } from '@angular/router';
 import { AuthService } from '../../service/auth.service';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
+import { User } from '../../Interfaces/user';
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -25,10 +26,14 @@ export class NavbarComponent implements OnInit {
   constructor(private sidebarService: SidebarService, private router: Router, private auth: AuthService) {
     this.isLoggedIn = this.auth.isLoggedIn();
   }
+  currentUser: User | null = null;
   ngOnInit(): void {
     this.router.events.subscribe(() => {
       this.isSidebarVisible = this.router.url.includes('admin');
     });
+    this.auth.getCurrentUser().subscribe(u =>{
+      this.currentUser = u;
+    })
   }
 
   homeNavigate(){
@@ -60,5 +65,13 @@ export class NavbarComponent implements OnInit {
     if(this.isSidebarVisible){
       this.sidebarService.toggleSidebar();
     }   
+  }
+
+  toProfile(){
+    this.router.navigate(['profile'])
+  }
+
+  toDashBoard(){
+    this.router.navigate(['admin'])
   }
 }
