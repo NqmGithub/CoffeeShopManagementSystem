d4e1846b530aec93952a7496a9421bc8ec7e934e
hungnd29523 <anhdoi7496@gmail.com>
2025-07-17 22:07:15 +0700

    scs

--- Front end/CoffeeShop/ConsoleApp2/obj/Debug/net9.0/ClientApp.AssemblyInfo.cs
+++ Front end/CoffeeShop/ConsoleApp2/obj/Debug/net9.0/ClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53c0ba0e1e264086f07dbc45e25df47e47052a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb19b889132b0b5e8057d3c981531f6143c03df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
